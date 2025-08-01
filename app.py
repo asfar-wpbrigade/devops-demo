0d4f3e77a453ca424aea080322a54c53aac84822
asfar-wpbrigade <asfar.wpbrigade@gmail.com>
2025-08-01 05:39:31 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello this is the changed text!"
+    return "Hello this is the pre-changed test"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
